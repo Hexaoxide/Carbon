938f857b86a0cdba822c213ee81a525c21fb3211
Draycia <lonelyyordle@gmail.com>
2020-06-15 20:47:17 -0700

    Add channel use permissions

--- src/main/java/net/draycia/simplechat/commands/ChannelCommand.java
+++ src/main/java/net/draycia/simplechat/commands/ChannelCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 @CommandAlias("ch|channel|switch")
+@CommandPermission("simplechat.use.switch")
 public class ChannelCommand extends BaseCommand {
 
     private SimpleChat simpleChat;
