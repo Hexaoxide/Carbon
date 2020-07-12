9b6e800c86f256cf940be014c3dc8cd8f5624f86
Draycia <lonelyyordle@gmail.com>
2020-07-12 06:11:01 -0700

    Register Moderation commands

--- SimpleChatModeration/src/main/java/net/draycia/simplechatmoderation/SimpleChatModeration.java
+++ SimpleChatModeration/src/main/java/net/draycia/simplechatmoderation/SimpleChatModeration.java
@@ -1,6 +1,10 @@
 package net.draycia.simplechatmoderation;
 
+import co.aikar.commands.CommandManager;
 import net.draycia.simplechat.SimpleChat;
+import net.draycia.simplechatmoderation.commands.ClearChatCommand;
+import net.draycia.simplechatmoderation.commands.MuteCommand;
+import net.draycia.simplechatmoderation.commands.ShadowMuteCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +15,12 @@ public final class SimpleChatModeration extends JavaPlugin {
     @Override
     public void onEnable() {
         simpleChat = (SimpleChat)Bukkit.getPluginManager().getPlugin("SimpleChat");
+
+        CommandManager commandManager = simpleChat.getCommandManager().getCommandManager();
+
+        commandManager.registerCommand(new ClearChatCommand(this));
+        commandManager.registerCommand(new MuteCommand(this));
+        commandManager.registerCommand(new ShadowMuteCommand(this));
     }
 
     @Override
