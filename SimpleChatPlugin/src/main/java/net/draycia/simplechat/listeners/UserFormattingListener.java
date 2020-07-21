69e52f3921f82860f5ac26eec7885ea254916d63
broccolai <josh@magmo.co.uk>
2020-07-21 02:22:40 +0100

    Fold ugly if

--- SimpleChatPlugin/src/main/java/net/draycia/simplechat/listeners/UserFormattingListener.java
+++ SimpleChatPlugin/src/main/java/net/draycia/simplechat/listeners/UserFormattingListener.java
@@ -8,13 +8,11 @@ public class UserFormattingListener implements Listener {
 
     @EventHandler
     public void onFormat(ChatFormatEvent event) {
-        if (event.getUser().isOnline()) {
-            if (event.getUser().asPlayer().hasPermission("simplechat.formatting")) {
-                event.setFormat(event.getFormat()
-                        .replace("<pre><message></pre>", "<message>")
-                        .replace("<pre><message>", "<message>")
-                );
-            }
+        if (!event.getUser().isOnline() || !event.getUser().asPlayer().hasPermission("simplechat.formatting")) {
+            event.setFormat(event.getFormat()
+                    .replace("<pre><message></pre>", "<message>")
+                    .replace("<pre><message>", "<message>")
+            );
         }
     }
 
