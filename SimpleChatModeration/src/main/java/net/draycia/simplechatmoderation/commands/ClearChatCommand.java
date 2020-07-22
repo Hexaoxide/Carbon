13207ee0aba8ea1d25f50835bd324f2bab5fae86
Draycia <lonelyyordle@gmail.com>
2020-07-22 07:18:06 -0700

    Fix clearchat command

--- SimpleChatModeration/src/main/java/net/draycia/simplechatmoderation/commands/ClearChatCommand.java
+++ SimpleChatModeration/src/main/java/net/draycia/simplechatmoderation/commands/ClearChatCommand.java
@@ -1,7 +1,6 @@
 package net.draycia.simplechatmoderation.commands;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.CommandIssuer;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
@@ -9,6 +8,7 @@ import net.draycia.simplechatmoderation.SimpleChatModeration;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandAlias("clearchat|cc")
@@ -22,7 +22,7 @@ public class ClearChatCommand extends BaseCommand {
     }
 
     @Default
-    public void baseCommand(CommandIssuer issuer) {
+    public void baseCommand(CommandSender issuer) {
         String format = moderation.getConfig().getString("clear-chat.message", "");
         Component component = moderation.getSimpleChat().getAdventureManager().processMessage(format, "br", "\n");
 
@@ -37,16 +37,14 @@ public class ClearChatCommand extends BaseCommand {
                 audience.sendMessage(component);
             }
 
-            String name = issuer.isPlayer() ? ((Player)issuer).getName() : "Console";
-
             if (player.hasPermission("simplechat.clearchat.notify")) {
                 String message = moderation.getConfig().getString("language.clear-notify");
-                audience.sendMessage(moderation.getSimpleChat().getAdventureManager().processMessage(message, "player", name));
+                audience.sendMessage(moderation.getSimpleChat().getAdventureManager().processMessage(message, "player", issuer.getName()));
             }
 
             if (player.hasPermission("simplechat.clearchat.exempt")) {
                 String message = moderation.getConfig().getString("language.clear-exempt");
-                audience.sendMessage(moderation.getSimpleChat().getAdventureManager().processMessage(message, "player", name));
+                audience.sendMessage(moderation.getSimpleChat().getAdventureManager().processMessage(message, "player", issuer.getName()));
             }
         }
     }
