3f5c267b6f40188f07af99fc4eba296445a959ee
Draycia <lonelyyordle@gmail.com>
2020-07-28 16:00:55 -0700

    Add worldguard-region:regionname context

--- CarbonChatWorldGuard/src/main/java/net/draycia/carbonworldguard/CarbonChatWorldGuard.java
+++ CarbonChatWorldGuard/src/main/java/net/draycia/carbonworldguard/CarbonChatWorldGuard.java
@@ -4,6 +4,7 @@ import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.util.Location;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
@@ -14,22 +15,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CarbonChatWorldGuard extends JavaPlugin {
 
-    private CarbonChat carbonChat;
-
     @Override
     public void onEnable() {
-        carbonChat = (CarbonChat) Bukkit.getPluginManager().getPlugin("CarbonChat");
+        CarbonChat carbonChat = (CarbonChat) Bukkit.getPluginManager().getPlugin("CarbonChat");
 
         carbonChat.getContextManager().register("worldguard-region", (context) -> {
             if ((context.getValue() instanceof Boolean) && ((Boolean) context.getValue())) {
                 return isInSameRegion(context.getSender(), context.getTarget());
             }
 
+            if (context.getValue() instanceof String) {
+                return isInSameRegion((String) context.getValue(), context.getSender(), context.getTarget());
+            }
+
             return true;
         });
     }
 
-    public boolean  isInSameRegion(ChatUser user1, ChatUser user2) {
+    public boolean isInSameRegion(ChatUser user1, ChatUser user2) {
         if (!user1.isOnline() || !user2.isOnline()) {
             return false;
         }
@@ -50,6 +53,37 @@ public final class CarbonChatWorldGuard extends JavaPlugin {
         }
 
         return false;
+    }
+
+    public boolean isInSameRegion(String region, ChatUser user1, ChatUser user2) {
+        if (!user1.isOnline() || !user2.isOnline()) {
+            return false;
+        }
+
+        RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
+        RegionManager regionManager = container.get(BukkitAdapter.adapt(user1.asPlayer().getWorld()));
+
+        if (regionManager == null) {
+            return false;
+        }
+
+        ProtectedRegion protection = regionManager.getRegion(region);
+
+        if (protection == null) {
+            return false;
+        }
+
+        if (!isInRegion(protection, user1)) {
+            return false;
+        }
+
+        return isInRegion(protection, user2);
+    }
+
+    public boolean isInRegion(ProtectedRegion region, ChatUser user) {
+        Location location = BukkitAdapter.adapt(user.asPlayer().getLocation());
+
+        return region.contains(location.getBlockX(), location.getBlockY(), location.getBlockZ());
     }
 
 }
