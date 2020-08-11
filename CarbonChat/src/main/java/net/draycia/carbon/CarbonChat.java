6d176b0aede18b233dd33dcd8ddbe9d917786e4c
Draycia <lonelyyordle@gmail.com>
2020-08-11 01:05:28 -0700

    Register event (enable previous feature)

--- CarbonChat/src/main/java/net/draycia/carbon/CarbonChat.java
+++ CarbonChat/src/main/java/net/draycia/carbon/CarbonChat.java
@@ -95,6 +95,7 @@ public final class CarbonChat extends JavaPlugin {
         pluginManager.registerEvents(new PlayerJoinListener(this), this);
         pluginManager.registerEvents(new RelationalPlaceholderHandler(), this);
         pluginManager.registerEvents(new UserFormattingListener(), this);
+        pluginManager.registerEvents(new WhisperPingHandler(this), this);
     }
 
     private void registerContexts() {
