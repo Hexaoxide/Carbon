f2c17d1800f5751e1c0a0ee678752d191ed32135
underscore11code <minecrafter11mrt@gmail.com>
2020-08-12 00:48:25 -0700

    Handle no eco plugin

--- CarbonChat/src/main/java/net/draycia/carbon/listeners/RequiredBalanceHandler.java
+++ CarbonChat/src/main/java/net/draycia/carbon/listeners/RequiredBalanceHandler.java
@@ -6,18 +6,29 @@ import net.milkbowl.vault.economy.Economy;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class RequiredBalanceHandler implements Listener {
     private final CarbonChat carbonChat;
     private final Economy economy;
+    private final boolean enabled;
     
     public RequiredBalanceHandler(CarbonChat carbonChat) {
         this.carbonChat = carbonChat;
-        this.economy = carbonChat.getServer().getServicesManager().getRegistration(Economy.class).getProvider();
+        RegisteredServiceProvider<Economy> registeredEco = carbonChat.getServer().getServicesManager().getRegistration(Economy.class);
+        if (registeredEco == null) {
+            carbonChat.getServer().getLogger().warning("No Vault-compatible Economy plugin detected! Economy features won't work!");
+            this.economy = null;
+            this.enabled = false;
+            return;
+        }
+        this.enabled = true;
+        this.economy = registeredEco.getProvider();
     }
     
     @EventHandler
     public void onChatReqBal(PreChatFormatEvent event) {
+        if (!enabled) return;
         Object requiredBalObject = event.getChannel().getContext("vault-balance");
         if (!(requiredBalObject instanceof Double)) return;
         Double requiredBal = (Double) requiredBalObject;
@@ -35,6 +46,7 @@ public class RequiredBalanceHandler implements Listener {
 
     @EventHandler
     public void onChatCost(PreChatFormatEvent event) {
+        if (!enabled) return;
         Object costObject = event.getChannel().getContext("vault-cost");
         if (!(costObject instanceof Double)) return;
         Double cost = (Double) costObject;
