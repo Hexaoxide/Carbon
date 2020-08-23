03be997159b144ed5f0c2fa3cfd4726ac5aaeddc
Larry <hey@laarryy.dev>
2020-08-22 19:28:05 -0600

    won't list unavailable channels if none + optimize imports

--- src/main/java/net/draycia/carbon/commands/ChannelListCommand.java
+++ src/main/java/net/draycia/carbon/commands/ChannelListCommand.java
@@ -9,7 +9,8 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.entity.Player;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -69,7 +70,7 @@ public class ChannelListCommand extends BaseCommand {
 
         carbonChat.getUserService().wrap(player).sendMessage(availableComponent);
 
-        if (player.hasPermission("carbonchat.channellist.bypass")) {
+        if (player.hasPermission("carbonchat.channellist.bypass") && !cannotSee.isEmpty()) {
 
             Iterator<ChatChannel> invisibleChannels = cannotSee.iterator();
             TextComponent.Builder unavailableList = TextComponent.builder("");
