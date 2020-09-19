f55af954854ecf1f9155f2f6c1f0fb7ae4c1bcdd
Draycia <lonelyyordle@gmail.com>
2020-09-19 00:12:09 -0700

    Fix legacy formatting handler

--- src/main/java/net/draycia/carbon/listeners/events/LegacyFormatHandler.java
+++ src/main/java/net/draycia/carbon/listeners/events/LegacyFormatHandler.java
@@ -1,18 +1,41 @@
 package net.draycia.carbon.listeners.events;
 
-import net.draycia.carbon.CarbonChat;
 import net.draycia.carbon.events.CarbonEvents;
 import net.draycia.carbon.events.api.PreChatFormatEvent;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.serializer.legacy.LegacyFormat;
 import net.kyori.event.PostOrders;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.regex.Pattern;
 
 public class LegacyFormatHandler {
 
+  private final @NonNull Pattern pattern =
+    Pattern.compile("[§&]x[§&]([0-9a-f])[§&]([0-9a-f])[§&]([0-9a-f])[§&]([0-9a-f])[§&]([0-9a-f])[§&]([0-9a-f])");
+
   public LegacyFormatHandler() {
     CarbonEvents.register(PreChatFormatEvent.class, PostOrders.FIRST, false, event -> {
-      final Component component = CarbonChat.LEGACY.deserialize(event.format());
-      event.format(MiniMessage.get().serialize(component));
+      // Legacy RGB
+      event.format(this.pattern.matcher(event.format()).replaceAll("<#$1$2$3$4$5$6>"));
+
+      // Legacy Colors
+      for (final char c : "0123456789abcdef".toCharArray()) {
+        final LegacyFormat format = LegacyComponentSerializer.parseChar(c);
+        final TextColor color = format.color();
+
+        event.format(event.format().replaceAll("[§&]" + c, "<" + color.asHexString() + ">"));
+      }
+
+      // Legacy Formatting
+      for (final char c : "klmno".toCharArray()) {
+        final LegacyFormat format = LegacyComponentSerializer.parseChar(c);
+        final TextDecoration decoration = format.decoration();
+
+        event.format(event.format().replaceAll("[§&]" + c, "<" + decoration.name() + ">"));
+      }
     });
   }
 
