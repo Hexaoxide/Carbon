046d5d9b28dc8374a3553885fa72054a896513d6
Draycia <lonelyyordle@gmail.com>
2021-05-27 09:36:13 -0700

    lol checkstyle

--- common/src/main/java/net/draycia/carbon/common/channels/BasicChatChannel.java
+++ common/src/main/java/net/draycia/carbon/common/channels/BasicChatChannel.java
@@ -30,7 +30,7 @@ public class BasicChatChannel implements ChatChannel {
     ) {
         // TODO: Once per-user is setup, use the service for the recipient?
         // TODO: Or, have the MessageSource do the per-use for us.
-        return service.basicChatFormat(
+        return this.service.basicChatFormat(
             recipient,
             sender.uuid(),
             sender.displayName(),
