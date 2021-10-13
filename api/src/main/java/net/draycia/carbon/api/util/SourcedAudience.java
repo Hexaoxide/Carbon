cb74812dfa546852da55268a3a13090ab36aab66
Draycia <lonelyyordle@gmail.com>
2021-10-12 20:29:41 -0700

    Fix whispers not using proper identity

--- api/src/main/java/net/draycia/carbon/api/util/SourcedAudience.java
+++ api/src/main/java/net/draycia/carbon/api/util/SourcedAudience.java
@@ -1,11 +1,20 @@
 package net.draycia.carbon.api.util;
 
 import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.audience.ForwardingAudience;
+import org.jetbrains.annotations.NotNull;
 
-public record SourcedAudience(Audience sender, Audience recipient) {
+public record SourcedAudience(Audience sender, Audience recipient) implements ForwardingAudience.Single {
+
+    public static final SourcedAudience EMPTY = new SourcedAudience(Audience.empty(), Audience.empty());
 
     public static SourcedAudience empty() {
-        return new SourcedAudience(Audience.empty(), Audience.empty());
+        return EMPTY;
+    }
+
+    @Override
+    public @NotNull Audience audience() {
+        return Audience.audience(sender, recipient);
     }
 
 }
