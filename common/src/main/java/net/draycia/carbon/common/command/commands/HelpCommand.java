4b65afb17e53dd43f9654d01418223743c064c41
darbyjack <admin@glaremasters.me>
2021-10-19 20:11:59 -0500

    /carbonhelp -> /carbon help

--- common/src/main/java/net/draycia/carbon/common/command/commands/HelpCommand.java
+++ common/src/main/java/net/draycia/carbon/common/command/commands/HelpCommand.java
@@ -13,12 +13,13 @@ public class HelpCommand {
     public HelpCommand(
         final CommandManager<Commander> commandManager
     ) {
-        var command = commandManager.commandBuilder("carbonhelp")
+        var command = commandManager.commandBuilder("carbon")
+            .literal("help")
             .argument(StringArgument.optional("query", StringArgument.StringMode.GREEDY))
             .permission("carbon.help")
             .senderType(Commander.class)
             .handler(handler -> {
-                var help = new MinecraftHelp<>("/carbonhelp", AudienceProvider.nativeAudience(), commandManager);
+                var help = new MinecraftHelp<>("/carbon help", AudienceProvider.nativeAudience(), commandManager);
                 help.queryCommands(handler.getOrDefault("query", ""), handler.getSender());
             })
             .build();
