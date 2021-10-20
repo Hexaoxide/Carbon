a78bca0efb97ea90b5ba96b301214309f4ebed57
Glare <admin@glaremasters.me>
2021-10-19 20:13:42 -0500

    Implemented basic Cloud-generated help command (#157)

---
+++ common/src/main/java/net/draycia/carbon/common/command/commands/HelpCommand.java
@@ -0,0 +1,29 @@
+package net.draycia.carbon.common.command.commands;
+
+import cloud.commandframework.CommandManager;
+import cloud.commandframework.arguments.standard.StringArgument;
+import cloud.commandframework.minecraft.extras.AudienceProvider;
+import cloud.commandframework.minecraft.extras.MinecraftHelp;
+import com.google.inject.Inject;
+import net.draycia.carbon.common.command.Commander;
+
+public class HelpCommand {
+
+    @Inject
+    public HelpCommand(
+        final CommandManager<Commander> commandManager
+    ) {
+        var command = commandManager.commandBuilder("carbon")
+            .literal("help")
+            .argument(StringArgument.optional("query", StringArgument.StringMode.GREEDY))
+            .permission("carbon.help")
+            .senderType(Commander.class)
+            .handler(handler -> {
+                var help = new MinecraftHelp<>("/carbon help", AudienceProvider.nativeAudience(), commandManager);
+                help.queryCommands(handler.getOrDefault("query", ""), handler.getSender());
+            })
+            .build();
+
+        commandManager.command(command);
+    }
+}
