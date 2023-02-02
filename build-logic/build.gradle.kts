f423b7413a074bd43925b8fe38bf701380791fb5
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-02-02 13:43:24 -0700

    Move permissions to common yml

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 repositories {
   gradlePluginPortal()
+  mavenCentral()
   maven("https://repo.stellardrift.ca/repository/snapshots/")
 }
 
@@ -12,6 +13,7 @@ dependencies {
   implementation(libs.indraCommon)
   implementation(libs.indraLicenseHeader)
   implementation(libs.licenser)
+  implementation("org.spongepowered:configurate-yaml:4.0.0")
 
   // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
