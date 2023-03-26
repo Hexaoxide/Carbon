f24a64c59adc14f3ca26b957bd6bdf46a1eb1ea4
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-03-26 10:36:26 -0700

    trim keys from database to remove null terminator padding

--- common/src/main/java/net/draycia/carbon/common/users/db/KeyColumnMapper.java
+++ common/src/main/java/net/draycia/carbon/common/users/db/KeyColumnMapper.java
@@ -22,6 +22,7 @@ package net.draycia.carbon.common.users.db;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import net.kyori.adventure.key.Key;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.intellij.lang.annotations.Subst;
 import org.jdbi.v3.core.mapper.ColumnMapper;
 import org.jdbi.v3.core.statement.StatementContext;
@@ -30,10 +31,11 @@ public class KeyColumnMapper implements ColumnMapper<Key> {
 
     @Override
     public Key map(final ResultSet rs, final int columnNumber, final StatementContext ctx) throws SQLException {
-        final @Subst("key:value") String keyValue = rs.getString(columnNumber);
+        final @Nullable String keyValue = rs.getString(columnNumber);
+        final @Nullable @Subst("key:value") String trimmed = keyValue == null ? null : keyValue.trim();
 
         if (keyValue != null) {
-            return Key.key(keyValue);
+            return Key.key(trimmed);
         }
 
         return null;
