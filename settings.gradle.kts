9673db40dfb21706876dd18c2d7d53074ffb4135
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-06-17 15:35:00 -0700

    Bump gradle & loom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -74,7 +74,7 @@ pluginManagement {
 
 plugins {
   id("ca.stellardrift.polyglot-version-catalogs") version "6.0.1"
-  id("quiet-fabric-loom") version "1.1-SNAPSHOT"
+  id("quiet-fabric-loom") version "1.2-SNAPSHOT"
 }
 
 rootProject.name = "CarbonChat"
