d9553bcfad15841e0d1aacb20ab46fae85b77bfa
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-10-05 22:11:44 -0700

    link external javadoc for dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,12 +4,25 @@ plugins {
 
 description = "API for interfacing with the CarbonChat Minecraft mod/plugin"
 
+val docs: Configuration by configurations.creating {
+  attributes {
+    attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
+    attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+    attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.SOURCES))
+    attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+  }
+}
+configurations.compileOnlyApi {
+  extendsFrom(docs)
+}
+
 dependencies {
   // Doesn't add any dependencies, only version constraints
   api(platform(libs.adventureBom))
+  docs(platform(libs.adventureBom))
 
   // Provided by platform
-  compileOnlyApi(libs.adventureApi)
+  docs(libs.adventureApi)
   compileOnlyApi(libs.adventureTextSerializerPlain)
   compileOnlyApi(libs.adventureTextSerializerLegacy)
   compileOnlyApi(libs.adventureTextSerializerGson) {
@@ -17,8 +30,23 @@ dependencies {
   }
   compileOnlyApi(libs.minimessage)
 
-  compileOnlyApi(libs.checkerQual)
+  docs(libs.checkerQual)
 
   // Provided by Minecraft
   compileOnlyApi(libs.gson)
+}
+
+tasks.withType<Javadoc> {
+  val options = options as StandardJavadocDocletOptions
+  options.links(
+    "https://jd.advntr.dev/api/${libs.versions.adventure.get()}/",
+    "https://javadocs.dev/org.checkerframework/checker-qual/${libs.versions.checkerQual.get()}/"
+  )
+  inputs.files(docs).ignoreEmptyDirectories().withPropertyName(docs.name + "-configuration")
+  doFirst {
+    options.addStringOption(
+      "sourcepath",
+      docs.resolvedConfiguration.files.joinToString(separator = File.pathSeparator, transform = File::getPath)
+    )
+  }
 }
