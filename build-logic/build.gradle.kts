17b0c5466aae09ac47205de8185cfb425402be88
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-11-05 23:01:30 -0700

    Adjust build-logic deps

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -12,12 +12,14 @@ dependencies {
   implementation(libs.shadow)
   implementation(libs.indraCommon)
   implementation(libs.indraLicenseHeader)
-  implementation(libs.pluginYml)
   implementation(libs.minotaur)
   implementation(libs.configurateYaml)
   implementation(libs.gremlin.gradle)
-  compileOnly("com.fasterxml.jackson.core:jackson-core:2.14.0")
-  compileOnly("com.fasterxml.jackson.core:jackson-annotations:2.14.0")
+
+  implementation(libs.pluginYml)
+  // Implementation dependencies of plugin-yml
+  implementation("com.fasterxml.jackson.core:jackson-core:2.15.3")
+  implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.3")
 
   // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
   implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
