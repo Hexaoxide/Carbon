7423c3e3e7f52dcf839b72419284d2c07622d6cf
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-11-07 23:18:58 -0700

    remove unused repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -43,8 +43,6 @@ dependencyResolutionManagement {
     // DiscordSRV
     maven("https://m2.dv8tion.net/releases")
     maven("https://nexus.scarsz.me/content/groups/public/")
-    // Libby (AlessioDP's fork)
-    maven("https://repo.alessiodp.com/releases/")
   }
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
