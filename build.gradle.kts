28736d31658b448ac3f9eab66d959dde828c503d
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-12-13 11:48:24 -0800

    Bump hangar-publish-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import io.papermc.hangarpublishplugin.model.Platforms
-
 plugins {
   id("carbon.build-logic")
   alias(libs.plugins.hangar.publish)
@@ -9,41 +7,44 @@ plugins {
 val projectVersion: String by project // get from gradle.properties
 version = projectVersion
 
+fun Project.platformJar(): Provider<RegularFile> =
+  extensions.getByType<CarbonPlatformExtension>().jarTask.flatMap { it.archiveFile }
+
 hangarPublish.publications.register("plugin") {
-  version.set(projectVersion)
-  id.set("Carbon")
-  channel.set(if (projectVersion.contains("-beta.")) "Beta" else "Release")
-  changelog.set(releaseNotes)
-  apiKey.set(providers.environmentVariable("HANGAR_UPLOAD_KEY"))
-  platforms.register(Platforms.PAPER) {
-    jar.set(project(":carbonchat-paper").the<CarbonPlatformExtension>().jarTask.flatMap { it.archiveFile })
+  version = projectVersion
+  id = "Carbon"
+  channel = if (projectVersion.contains("-beta.")) "Beta" else "Release"
+  changelog = releaseNotes
+  apiKey = providers.environmentVariable("HANGAR_UPLOAD_KEY")
+  platforms.paper {
+    jar = project(":carbonchat-paper").platformJar()
     platformVersions.add("1.19.4-1.20.2")
     dependencies {
       url("LuckPerms", "https://luckperms.net/")
       hangar("Essentials") {
-        required.set(false)
+        required = false
       }
       url("DiscordSRV", "https://www.spigotmc.org/resources/discordsrv.18494/") {
-        required.set(false)
+        required = false
       }
       url("PlaceholderAPI", "https://www.spigotmc.org/resources/placeholderapi.6245/") {
-        required.set(false)
+        required = false
       }
       hangar("MiniPlaceholders") {
-        required.set(false)
+        required = false
       }
     }
   }
-  platforms.register(Platforms.VELOCITY) {
-    jar.set(project(":carbonchat-velocity").the<CarbonPlatformExtension>().jarTask.flatMap { it.archiveFile })
+  platforms.velocity {
+    jar = project(":carbonchat-velocity").platformJar()
     platformVersions.add("3.2")
     dependencies {
       url("LuckPerms", "https://luckperms.net/")
       hangar("MiniPlaceholders") {
-        required.set(false)
+        required = false
       }
       hangar("SignedVelocity") {
-        required.set(false)
+        required = false
       }
     }
   }
