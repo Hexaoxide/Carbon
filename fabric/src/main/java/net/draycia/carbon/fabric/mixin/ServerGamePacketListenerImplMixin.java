e548e176d67153ecd77b79077012d44c90c9c8d9
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-01 18:20:00 -0800

    Update LICENSE_HEADER

--- fabric/src/main/java/net/draycia/carbon/fabric/mixin/ServerGamePacketListenerImplMixin.java
+++ fabric/src/main/java/net/draycia/carbon/fabric/mixin/ServerGamePacketListenerImplMixin.java
@@ -1,7 +1,7 @@
 /*
  * CarbonChat
  *
- * Copyright (c) 2023 Josua Parks (Vicarious)
+ * Copyright (c) 2024 Josua Parks (Vicarious)
  *                    Contributors
  *
  * This program is free software: you can redistribute it and/or modify
