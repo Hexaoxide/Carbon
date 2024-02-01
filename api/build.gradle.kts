26e98acc59cc06db7b3a40f72217f047e63a2b83
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-31 18:51:16 -0700

    Use cloud-build-logic.javadoc-links for automatic javadoc linking

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,28 +1,16 @@
 plugins {
   id("carbon.publishing-conventions")
+  id("org.incendo.cloud-build-logic.javadoc-links") version "0.0.10"
 }
 
 description = "API for interfacing with the CarbonChat Minecraft mod/plugin"
 
-val docs: Configuration by configurations.creating {
-  attributes {
-    attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.DOCUMENTATION))
-    attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-    attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.SOURCES))
-    attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
-  }
-}
-configurations.compileOnlyApi {
-  extendsFrom(docs)
-}
-
 dependencies {
   // Doesn't add any dependencies, only version constraints
   api(platform(libs.adventureBom))
-  docs(platform(libs.adventureBom))
 
   // Provided by platform
-  docs(libs.adventureApi)
+  compileOnlyApi(libs.adventureApi)
   compileOnlyApi(libs.adventureTextSerializerPlain)
   compileOnlyApi(libs.adventureTextSerializerLegacy)
   compileOnlyApi(libs.adventureTextSerializerGson) {
@@ -30,23 +18,8 @@ dependencies {
   }
   compileOnlyApi(libs.minimessage)
 
-  docs(libs.checkerQual)
+  compileOnlyApi(libs.checkerQual)
 
   // Provided by Minecraft
   compileOnlyApi(libs.gson)
-}
-
-tasks.withType<Javadoc> {
-  val options = options as StandardJavadocDocletOptions
-  options.links(
-    "https://jd.advntr.dev/api/${libs.versions.adventure.get()}/",
-    "https://javadoc.io/doc/org.checkerframework/checker-qual/${libs.versions.checkerQual.get()}/",
-  )
-  inputs.files(docs).ignoreEmptyDirectories().withPropertyName(docs.name + "-configuration")
-  doFirst {
-    options.addStringOption(
-      "sourcepath",
-      docs.resolvedConfiguration.files.joinToString(separator = File.pathSeparator, transform = File::getPath)
-    )
-  }
 }
