b47bb7a8f530c7a2512948af40bd74879c501b2f
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-01-23 12:57:47 -0700

    Fix style

--- neoforge/src/main/java/net/draycia/carbon/neoforge/CarbonChatNeoForge.java
+++ neoforge/src/main/java/net/draycia/carbon/neoforge/CarbonChatNeoForge.java
@@ -1,3 +1,22 @@
+/*
+ * CarbonChat
+ *
+ * Copyright (c) 2024 Josua Parks (Vicarious)
+ *                    Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package net.draycia.carbon.neoforge;
 
 import net.neoforged.bus.api.IEventBus;
@@ -5,6 +24,7 @@ import net.neoforged.fml.common.Mod;
 
 @Mod("carbonchat")
 public final class CarbonChatNeoForge {
+
     public CarbonChatNeoForge(final IEventBus modBus) {
         System.out.println("Hello, carbonchat!");
     }
