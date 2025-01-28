ab00382ab0170e31a2844660fb01923d7575cf8e
Draycia <lonelyyordle@gmail.com>
2025-01-28 00:18:52 -0800

    Simplify party getter
    All versios of mcMMO seem to support the ewer method

--- paper/src/main/java/net/draycia/carbon/paper/integration/mcmmo/McmmoPartyChannel.java
+++ paper/src/main/java/net/draycia/carbon/paper/integration/mcmmo/McmmoPartyChannel.java
@@ -20,9 +20,7 @@
 package net.draycia.carbon.paper.integration.mcmmo;
 
 import com.gmail.nossr50.datatypes.party.Party;
-import com.gmail.nossr50.party.PartyManager;
 import com.google.inject.Inject;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -99,14 +97,7 @@ public class McmmoPartyChannel extends ConfigChatChannel {
     }
 
     private @Nullable Party party(final CarbonPlayer player) {
-        try {
-            // mcMMO < 2.2
-            final Method getParty = PartyManager.class.getMethod("getParty", Player.class);
-            return (Party) getParty.invoke(null, Bukkit.getPlayer(player.uuid()));
-        } catch (final Exception exception) {
-            // mcMMO 2.2
-            return com.gmail.nossr50.util.player.UserManager.getPlayer(Bukkit.getPlayer(player.uuid())).getParty();
-        }
+        return com.gmail.nossr50.util.player.UserManager.getPlayer(Bukkit.getPlayer(player.uuid())).getParty();
     }
 
 }
