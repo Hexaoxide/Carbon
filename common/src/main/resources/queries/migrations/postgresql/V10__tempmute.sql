03f3648ab815481d98c738c2a1936f245254b531
PseudoResonance <75700a85-1205-4740-aebf-4413a352e81b@otake.pw>
2025-05-18 17:41:06 -0700

    Fix #652 - PostgreSQL migrations V9 and V10

--- common/src/main/resources/queries/migrations/postgresql/V10__tempmute.sql
+++ common/src/main/resources/queries/migrations/postgresql/V10__tempmute.sql
@@ -1,1 +1,1 @@
-ALTER TABLE carbon_users ALTER COLUMN muteexpiration TYPE BIGINT;
+ALTER TABLE carbon_users ALTER COLUMN muteexpiration TYPE BIGINT USING (CASE WHEN muteexpiration THEN 1 ELSE 0 END);
