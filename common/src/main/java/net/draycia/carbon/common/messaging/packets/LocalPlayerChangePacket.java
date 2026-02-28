2fab19d32579c85452e25beaa8658c8f02c2acce
Josua Parks <lonelyyordle@gmail.com>
2026-02-27 22:20:27 -0800

    Avoid using default methods with null values (#754)

--- common/src/main/java/net/draycia/carbon/common/messaging/packets/LocalPlayerChangePacket.java
+++ common/src/main/java/net/draycia/carbon/common/messaging/packets/LocalPlayerChangePacket.java
@@ -52,6 +52,14 @@ public final class LocalPlayerChangePacket extends CarbonPacket {
         this.changeType = changeType;
     }
 
+    @AssistedInject
+    public LocalPlayerChangePacket(final @ServerId UUID serverId, final @Assisted UUID playerId) {
+        super(serverId);
+        this.playerId = playerId;
+        this.playerName = null;
+        this.changeType = ChangeType.REMOVE;
+    }
+
     public LocalPlayerChangePacket(final UUID sender, final ByteBuf data) {
         super(sender);
         this.read(data);
