b46aa0dac5a122f756f2b2444c2ff185f6246fc1
andreypidd <160744245+andreypidd@users.noreply.github.com>
2026-03-30 19:03:34 +0300

    Update CarbonPlayerFabric.java

--- fabric/src/main/java/net/draycia/carbon/fabric/users/CarbonPlayerFabric.java
+++ fabric/src/main/java/net/draycia/carbon/fabric/users/CarbonPlayerFabric.java
@@ -129,7 +129,7 @@ public class CarbonPlayerFabric extends WrappedCarbonPlayer implements Forwardin
     @Override
     public @Nullable String worldName() {
         return this.player()
-            .map(p -> p.level().dimension().location().toString())
+            .map(p -> p.level().dimension().toString())
             .orElse(null);
     }
 
